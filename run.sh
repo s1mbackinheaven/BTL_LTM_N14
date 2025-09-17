4f922a7d33e356003ddd1b489356a7fd8b2032a7
dungcony <dung090804@gmail.com>
2025-09-17 08:16:01 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+# Tạo database MySQL trong Docker
+docker-compose -f docker-compose.mysql.yml up -d
+
+
 mvn clean compile exec:java -Dexec.mainClass="com.oop.game.HelloApplication" -Dexec.args="--add-exports javafx.base/com.sun.javafx.runtime=ALL-UNNAMED"
